800a4655c75d54c31b74797360f69b810d603515
Ivana Yantra <yantrai@mail.ru>
2023-11-01 01:01:49 +0300

    init commit

---
+++ src/test/java/ru/netology/test/SqlLoginTest.java
@@ -0,0 +1,31 @@
+package ru.netology.test;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Test;
+import ru.netology.data.DataHelper;
+import ru.netology.data.SQLHelper;
+import ru.netology.page.LoginPage;
+
+import java.sql.SQLException;
+
+import static com.codeborne.selenide.Selenide.open;
+import static ru.netology.data.SQLHelper.cleanDatabase;
+
+public class SqlLoginTest {
+
+    @AfterAll
+    static void teardown() {
+        cleanDatabase();
+    }
+
+    @Test
+    void shouldSuccessFullLogin() throws SQLException {
+        open("http://localhost:9999");
+        var loginPage = new LoginPage();
+        var authInfo = DataHelper.getAuthInfo();
+        var verificationPage = loginPage.validLogin(authInfo);
+        verificationPage.verificationPageVisiblity();
+        var verificationCode = SQLHelper.getVerifacationCode();
+        verificationPage.validVerify(verificationCode.getCode());
+    }
+}
